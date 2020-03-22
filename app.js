50e4f2009d6bc35b853c1ba12540af3b55873355
Chris Hesterman <chrishesterman14@gmail.com>
2020-03-22 13:51:20 -0700

    First react component rendered

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// TODO
+var GroceryList = () => {
+  return (
+    <div>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+    </div>
+  );
+};
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
