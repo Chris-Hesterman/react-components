f2abe57e431b3da1c0881a2ff4ffb3f6130232fb
Chris Hesterman <chrishesterman14@gmail.com>
2020-03-22 15:39:48 -0700

    Make list item bold when hovering over it

--- app.js
+++ app.js
@@ -27,11 +27,39 @@ var Pizza = () => {
 class GroceryListItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      done: false,
+    }
+  }
+
+  onListItemClick() {
+    this.setState({ done: !this.state.done });
+  }
+
+  onHoverListItem(event) {
+    event.target.style.fontWeight = '700';
+  }
+
+  onStopHoverListItem(event) {
+    event.target.style.fontWeight = '300';
   }
 
   render() {
+    var style1 = {
+      textDecoration: this.state.done ? 'line-through': 'none',
+      fontSize: '5rem',
+      listStyleType: 'none'
+    };
     return (
-      <li>{this.props.item}</li>
+      <li
+        key={this.props.item}
+        style={style1}
+        onClick={this.onListItemClick.bind(this)}
+        onMouseEnter={this.onHoverListItem.bind(this)}
+        onMouseLeave={this.onStopHoverListItem.bind(this)}
+      >
+        {this.props.item}
+      </li>
     );
   }
 }
